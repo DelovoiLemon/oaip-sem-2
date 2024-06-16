b397a7b111dd4c0918cc98623db8e82e14129593
delovoilemon <delovoilemon@gmail.com>
2024-06-17 02:38:45 +0300

    Some fixes, add menu for 2, 3, 5 labs

--- lab2/task.c
+++ lab2/task.c
@@ -1,124 +1,147 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 struct Stack {
-	double data;
-	struct Stack *next;	
+  double data;
+  struct Stack *next;
 };
 
-void push(struct Stack **s, double val)
-{
-	struct Stack *t = malloc(sizeof(struct Stack));
-	t->next = *s;
-	t->data = val;
-	*s = t;
+void push(struct Stack **s, double val) {
+  struct Stack *t = malloc(sizeof(struct Stack));
+  t->next = *s;
+  t->data = val;
+  *s = t;
 }
 
-double pop(struct Stack **s, double *val)
-{
-	struct Stack *t;
-	if(*s == NULL) return -1;
-	*val = (*s)->data;
-	t = *s;
-	*s = t->next;
-	free(t);
-	return 0;
+double pop(struct Stack **s, double *val) {
+  struct Stack *t;
+  if (*s == NULL)
+    return -1;
+  *val = (*s)->data;
+  t = *s;
+  *s = t->next;
+  free(t);
+  return 0;
 }
 
-void sort_p(struct Stack **s)
-{
-	double tmp;
-	struct Stack *t = NULL, *t1, *r;
-	push(s, 0);
-	if((*s)->next->next == NULL) return;
-	do {
-		for(t1 = *s; t1->next->next != t; t1 = t1->next)
-			if(t1->next->data > t1->next->next->data) {
-				r = t1->next->next;
-				t1->next->next = r->next;
-				r->next = t1->next;
-				t1->next = r;
-			}
-		t = t1->next;
-	} while((*s)->next->next != t);
-	pop(s, &tmp);
-	
+void sort_p(struct Stack **s) {
+  double tmp;
+  struct Stack *t = NULL, *t1, *r;
+  push(s, 0);
+  if ((*s)->next->next == NULL)
+    return;
+  do {
+    for (t1 = *s; t1->next->next != t; t1 = t1->next)
+      if (t1->next->data > t1->next->next->data) {
+        r = t1->next->next;
+        t1->next->next = r->next;
+        r->next = t1->next;
+        t1->next = r;
+      }
+    t = t1->next;
+  } while ((*s)->next->next != t);
+  pop(s, &tmp);
 }
 
-void sort_data(struct Stack *s)
-{
-	struct Stack *t = NULL, *t1; double r;
-	do {
-		for(t1 = s; t1->next != t; t1 = t1->next)
-			if(t1->data > t1->next->data) {
-				r = t1->data;
-				t1->data = t1->next->data;
-				t1->next->data = r;
-			}
-		t = t1;
-	} while(s->next != t);
+void sort_data(struct Stack *s) {
+  struct Stack *t = NULL, *t1;
+  double r;
+  do {
+    for (t1 = s; t1->next != t; t1 = t1->next)
+      if (t1->data > t1->next->data) {
+        r = t1->data;
+        t1->data = t1->next->data;
+        t1->next->data = r;
+      }
+    t = t1;
+  } while (s->next != t);
 }
 
 int print_stack(struct Stack *s) {
-	int i;
-	for(i = 0; s; s = s->next, i++)
-		printf("%i: %lf\n", i, s->data);
-	puts("\n");
-	return i;
+  int i;
+  for (i = 0; s; s = s->next, i++)
+    printf("%i: %lf\n", i, s->data);
+  puts("\n");
+  return i;
 }
 
-void del_all(struct Stack **s)
-{
-	struct Stack *t;
-	while(*s != NULL)
-	{
-		t = *s;
-		*s = t->next;
-		free(t);
-	}
+void del_all(struct Stack **s) {
+  struct Stack *t;
+  while (*s != NULL) {
+    t = *s;
+    *s = t->next;
+    free(t);
+  }
 }
 
-double avg_calc(struct Stack *s)
-{
-	int i; double sum = 0;
-	for(i = 0; s; i++, s = s->next)
-		sum += s->data;
-	return sum / i;
+double avg_calc(struct Stack *s) {
+  int i;
+  double sum = 0;
+  for (i = 0; s; i++, s = s->next)
+    sum += s->data;
+  return sum / i;
 }
 
 void task(struct Stack *s) {
-	if(!s) return;
+  if (!s)
+    return;
 
-	s->data = avg_calc(s);
+  s->data = avg_calc(s);
 }
 
-int main() 
-{
-	struct Stack *begin = NULL;
-
-	for(int i = 1; i <= 10; i++)
-		push(&begin, rand() % 100);
-	
-	puts("Исходный стек:\n");
-	print_stack(begin);
-	sort_data(begin);
-	puts("Отсортированный стек:\n");
-	print_stack(begin);
-	del_all(&begin);
-
-	for(int i = 1; i <= 25; i++)
-		push(&begin, rand() % 100);
-
-
-	puts("Исходный стек:\n");
-	print_stack(begin);
-	sort_p(&begin);
-	puts("Отсортированный стек:\n");
-	print_stack(begin);
-
-	task(begin);
-	puts("Задание:\n");
-	print_stack(begin);
-
-	del_all(&begin);
+int main() {
+  struct Stack *begin = NULL;
+  srand(time(NULL));
+  int choice;
+  double val;
+  while (1) {
+    printf("Menu:\n");
+    printf("1. Push a number onto the stack\n");
+    printf("2. Pop a number from the stack\n");
+    printf("3. Push random numbers onto the stack\n");
+    printf("4. Sort the stack using bubble sort\n");
+    printf("5. Sort the stack using insertion sort\n");
+    printf(
+        "6. Perform task (set the top element to the average of the stack)\n");
+    printf("7. Print the stack\n");
+    printf("8. Exit\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+    switch (choice) {
+    case 1:
+      printf("Enter a number to push: ");
+      scanf("%lf", &val);
+      push(&begin, val);
+      break;
+    case 2:
+      if (pop(&begin, &val) == 0)
+        printf("Popped: %lf\n", val);
+      else
+        printf("Stack is empty\n");
+      break;
+    case 3:
+      for (int i = 1; i <= 10; i++)
+        push(&begin, rand() % 100);
+      break;
+    case 4:
+      sort_data(begin);
+      break;
+    case 5:
+      sort_p(&begin);
+      break;
+    case 6:
+      task(begin);
+      break;
+    case 7:
+      print_stack(begin);
+      break;
+    case 8:
+      del_all(&begin);
+      return 0;
+    default:
+      printf("Invalid choice. Please try again.\n");
+    }
+  }
+  return 0;
 }
--- lab3/task.c
+++ lab3/task.c
@@ -1,117 +1,147 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct List {
-	double data;
-	struct List *next, *prev;	
+  double data;
+  struct List *next, *prev;
 } list;
 
-list *create_list(double data)
-{
-    list *t = malloc(sizeof(list));
-    t->data = data;
-    t->prev = t->next = NULL;
-    return t;
+list *create_list(double data) {
+  list *t = malloc(sizeof(list));
+  t->data = data;
+  t->prev = t->next = NULL;
+  return t;
 }
 
-void init(list **b, list **e, double val)
-{
-    *b = *e = create_list(val);
-}
-
+void init(list **b, list **e, double val) { *b = *e = create_list(val); }
 
 // derection (0 - start, anything - end);
-void add(int direction, list **b, list **e, double val)
-{
-    list *t = malloc(sizeof(list));
-    t->data = val;
-    if(direction) { // start
-        t->prev = NULL;
-        t->next = *b;
-        (*b)->prev = t;
-        *b = t;
-    } else { // end
-        t->next = NULL;
-        t->prev = *e;
-        (*e)->next = t;
-        (*e) = t;
-    }
+void add(int direction, list **b, list **e, double val) {
+  list *t = malloc(sizeof(list));
+  t->data = val;
+  if (direction) { // start
+    t->prev = NULL;
+    t->next = *b;
+    (*b)->prev = t;
+    *b = t;
+  } else { // end
+    t->next = NULL;
+    t->prev = *e;
+    (*e)->next = t;
+    (*e) = t;
+  }
 }
 
-int del_all(list **b)
-{
-    int i; list *t;
-    for(i = 0; *b; i++)
-    {
-        t = *b;
-        *b = t->next;
-        free(t);
-    }
-    return i;
+int del_all(list **b) {
+  int i;
+  list *t;
+  for (i = 0; *b; i++) {
+    t = *b;
+    *b = t->next;
+    free(t);
+  }
+  return i;
 }
-
 
 void swap_data(list *p1, list *p2) {
-    double tmp;
-    if(!p1 || !p2) return;
-    tmp = p1->data;
-    p1->data = p2->data;
-    p2->data = tmp;
+  double tmp;
+  if (!p1 || !p2)
+    return;
+  tmp = p1->data;
+  p1->data = p2->data;
+  p2->data = tmp;
 }
 
-void sort_data(list *s)
-{
-	list *t = NULL, *t1; double r;
-	do {
-		for(t1 = s; t1->next != t; t1 = t1->next)
-			if(t1->data < t1->next->data) {
-				swap_data(t1, t1->next);
-			}
-		t = t1;
-	} while(s->next != t);
+void sort_data(list *s) {
+  list *t = NULL, *t1;
+  double r;
+  do {
+    for (t1 = s; t1->next != t; t1 = t1->next)
+      if (t1->data < t1->next->data) {
+        swap_data(t1, t1->next);
+      }
+    t = t1;
+  } while (s->next != t);
 }
 
 int print_list(int direction, list *b, list *e) {
-	int i;
-    if(direction) for(i = 0; b; b = b->next, i++)
-            printf("%i: %lf\n", i, b->data);
-    else for(i = 0; e; e = e->prev, i++)
-            printf("%i: %lf\n", i, e->data);
-    
- 	return i;
+  int i;
+  if (direction)
+    for (i = 0; b; b = b->next, i++)
+      printf("%i: %lf\n", i, b->data);
+  else
+    for (i = 0; e; e = e->prev, i++)
+      printf("%i: %lf\n", i, e->data);
+
+  return i;
 }
 
-double avg_calc(list *s)
-{
-	int i; double sum = 0;
-	for(i = 0; s; i++, s = s->next)
-		sum += s->data;
-	return sum / i;
+double avg_calc(list *s) {
+  int i;
+  double sum = 0;
+  for (i = 0; s; i++, s = s->next)
+    sum += s->data;
+  return sum / i;
 }
 
 void task(list *b, list *e) {
-	if(!b) return;
-	e->data = avg_calc(b);
+  if (!b)
+    return;
+  e->data = avg_calc(b);
 }
 
-int main() 
-{
-	list *begin = NULL, *end = NULL;
-
-    init(&begin, &end, rand() % 1000);
-
-	for(int i = 0; i < 9; i++)
-		add(0, &begin, &end, rand() % 100);
-	
-	puts("Исходный стек:\n");
-	print_list(0, begin, end);
-	sort_data(begin);
-	puts("Отсортированный стек:\n");
-	print_list(0, begin, end);
-
-	task(begin, end);
-	puts("Задание:\n");
-	print_list(0, begin, end);
-
-	del_all(&begin);
+int main() {
+  list *begin = NULL, *end = NULL;
+  srand(time(NULL));
+  int choice;
+  double val;
+  while (1) {
+    printf("Menu:\n");
+    printf("1. Initialize list\n");
+    printf("2. Add element to the beginning\n");
+    printf("3. Add element to the end\n");
+    printf("4. Delete all elements\n");
+    printf("5. Sort list\n");
+    printf("6. Calculate average\n");
+    printf("7. Print list\n");
+    printf("8. Exit\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+    switch (choice) {
+    case 1:
+      printf("Enter value: ");
+      scanf("%lf", &val);
+      init(&begin, &end, val);
+      break;
+    case 2:
+      printf("Enter value: ");
+      scanf("%lf", &val);
+      add(1, &begin, &end, val);
+      break;
+    case 3:
+      printf("Enter value: ");
+      scanf("%lf", &val);
+      add(0, &begin, &end, val);
+      break;
+    case 4:
+      del_all(&begin);
+      begin = end = NULL;
+      break;
+    case 5:
+      sort_data(begin);
+      break;
+    case 6:
+      task(begin, end);
+      break;
+    case 7:
+      print_list(0, begin, end);
+      break;
+    case 8:
+      return 0;
+    default:
+      printf("Invalid choice\n");
+    }
+  }
+  return 0;
 }
